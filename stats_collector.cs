bc5f353198fcbc0fe3737df700c5c6e0014649db
jalho <alho.jere@gmail.com>
2024-03-03 22:01:51 +0200

    Distinguish suicides

--- stats_collector.cs
+++ stats_collector.cs
@@ -92,6 +92,19 @@ namespace Carbon.Plugins {
                 player_lines.Add(pve_death_event_serialized);
                 return (object) null;
             } else {
+                // case suicide
+                if (killed_player.userID == killer_info.InitiatorPlayer.userID) {
+                    StatsAccumulationEvent suicide_event = new StatsAccumulationEvent {
+                        timestamp = timestamp,
+                        type = "suicide",
+                        subject_id = killed_player.userID,
+                    };
+                    string suicide_event_serialized = JsonConvert.SerializeObject(suicide_event);
+                    var lines = this.aggregated_lines.GetOrAdd(killer_info.InitiatorPlayer.userID, _ => new List<string>());
+                    lines.Add(suicide_event_serialized);
+                    return (object) null;
+                }
+
                 // PvP kill event for killer player
                 StatsAccumulationEvent pvp_kill_event = new StatsAccumulationEvent {
                     timestamp = timestamp,
@@ -117,6 +130,7 @@ namespace Carbon.Plugins {
 
         }
 
+        // TODO: Accumulate stats from OnGrowableGather!
         object OnGrowableGather(GrowableEntity growable_entity) {
             Puts("OnGrowableGather was called!");
             return (object) null;
