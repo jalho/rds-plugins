1c3cb6b5023cbb8c3f004e8357725691bd6a0dc1
jalho <alho.jere@gmail.com>
2024-04-17 15:31:22 +0300

    Add method write_sock

--- stats_collector_unixsock.cs
+++ stats_collector_unixsock.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Text;
+using System.Net.Sockets;
 
 /* For reference implementation of a stats collecting plugin, see
    Statistics DB by misticos: https://umod.org/plugins/statistics-db */
@@ -17,9 +19,9 @@ namespace Carbon.Plugins {
          * Docs: https://docs.carbonmod.gg/docs/core/hooks/server
          * [Accessed 2024-04-17]
          */ 
-        void OnTick()
-        {
-            this.log("OnTick was called!");
+        void OnTick() {
+            // this.log("OnTick was called!");
+            this.write_sock("/tmp/asdasd.sock", "hello from tick!");
         }
 
         /**
@@ -32,6 +34,19 @@ namespace Carbon.Plugins {
         private void log(string message) {
             string timestamp_iso = DateTime.UtcNow.ToString("o");
             System.Console.WriteLine($"[{timestamp_iso}] {this.plugin_name}: {message}");
+        }
+
+        private void write_sock(string socket_fs_path, string message) {
+            UnixDomainSocketEndPoint endpoint = new UnixDomainSocketEndPoint(socket_fs_path);
+            try {
+                using (Socket socket = new Socket(AddressFamily.Unix, SocketType.Stream, ProtocolType.Unspecified)) {
+                    socket.Connect(endpoint);
+                    byte[] data = Encoding.UTF8.GetBytes(message);
+                    socket.Send(data);
+                }
+            } catch (Exception ex) {
+                Console.WriteLine($"Error writing to Unix domain socket: {ex.Message}");
+            }
         }
     }
 }
