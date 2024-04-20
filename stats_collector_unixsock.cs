bc37e5408cf42ced0fe69bd14f62eaf9b4bd0376
jalho <alho.jere@gmail.com>
2024-04-20 14:08:53 +0300

    Add debug helper method inspect_object

--- stats_collector_unixsock.cs
+++ stats_collector_unixsock.cs
@@ -6,6 +6,7 @@ using System;
 using System.Text;
 using Newtonsoft.Json;
 using System.Net.Sockets;
+using System.Reflection;
 
 class JSONSerializable {
     public virtual string to_json() {
@@ -84,6 +85,7 @@ namespace Carbon.Plugins {
         }
 
         object OnCargoShipSpawnCrate(CargoShip self) {
+            // this.inspect_object(self);
             long timestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
             var world_event = new WorldEvent {
                 timestamp = (ulong) timestamp,
@@ -91,6 +93,20 @@ namespace Carbon.Plugins {
             };
             this.write_sock(world_event);
             return (object) null;
+        }
+
+        /**
+         * Debug helper method.
+         */
+        private void inspect_object(object inspectable) {
+            Type inspectable_type = inspectable.GetType();
+            PropertyInfo[] properties = inspectable_type.GetProperties();
+            StringBuilder property_names = new StringBuilder();
+            foreach (PropertyInfo property in properties)
+            {
+                property_names.Append(property.Name + "\n\t");
+            }
+            Console.WriteLine($"FullName: '{inspectable_type.FullName}', Property Names:\n\t{property_names}");
         }
 
         /**
